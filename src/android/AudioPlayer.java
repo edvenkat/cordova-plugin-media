9b14f14e426825c24208f8ddfc94efd8326bec28
Venka Tesh G <gvenkatesh225@gmail.com>
2022-09-09 18:04:40 +0530

    Update AudioPlayer.java

--- src/android/AudioPlayer.java
+++ src/android/AudioPlayer.java
@@ -156,7 +156,7 @@ public class AudioPlayer implements OnCompletionListener, OnPreparedListener, On
             this.recorder.setAudioSource(MediaRecorder.AudioSource.VOICE_RECOGNITION);
             this.recorder.setOutputFormat(MediaRecorder.OutputFormat.AAC_ADTS); // RAW_AMR);
             this.recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC); //AMR_NB);
-            this.recorder.setMaxDuration(600000);
+//             this.recorder.setMaxDuration(600000);
             this.tempFile = generateTempFile();
             this.recorder.setOutputFile(this.tempFile);
             try {
